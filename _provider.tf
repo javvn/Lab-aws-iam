a89eb762cc0ba519613c034d13a396f7a6f96bcd
javvn <juho0428@naver.com>
2023-01-27 23:33:25 +0900

    edit

--- _provider.tf
+++ _provider.tf
@@ -1,10 +1,3 @@
-terraform {
-  required_version = "1.3.5"
-
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~>4.0"
-    }
-  }
+provider "aws" {
+  region = "us-east-1"
 }
--- _version.tf
+++ _version.tf
@@ -1,3 +1,10 @@
-provider "aws" {
-  region = "ap-northeast-2"
+terraform {
+  required_version = "1.3.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.0"
+    }
+  }
 }
